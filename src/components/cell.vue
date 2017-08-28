f05d52994a2f62444973d9f3a63889e583b9d1de
Daniel Cross <dancross09@gmail.com>
2017-08-28 16:12:28 -0400

    First implementation of cell class

---
+++ src/components/cell.vue
@@ -0,0 +1,37 @@
+<template>
+    <div v-on:click="FlipIsAlive" />
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            isAlive: false
+        };
+    },
+    methods: {
+        FlipIsAlive: function() {
+            this.isAlive = !this.isAlive;
+
+            if (this.isAlive) {
+                console.log("flipped to life!");
+                this.$el.style.backgroundColor = "#22d822";
+            }
+            else {
+                console.log("flipped to death");
+                this.$el.style.backgroundColor = "#3c4951";
+            }
+        }
+    }
+};
+</script>
+
+<style>
+    div {
+        width: 20px;
+        height: 20px;
+        background-color: #3c4951;
+        /*background-color: #80c8ed;*/
+        margin: 4px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 // import App from './App.vue';
-import Parent from "./components/parent.vue";
+import Cell from "./components/cell.vue";
+
 
 new Vue({
   el: '#app',
-  render: h => h(Parent)
+  render: h => h(Cell)
 });
